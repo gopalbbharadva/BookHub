52404a801f67ddc588a94b1fd5774b20becb0118
gopal <gopalbbharadva@gmail.com>
2022-03-16 21:43:39 +0530

    Refactor : Main route component created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import MockmanEs from "mockman-js";
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./App.css";
-import {
-  Login,
-  Signup,
-  Products,
-  Wishlist,
-  Cart,
-  Home,
-} from "./barrelexport/Pageutil";
+import { AppRoute } from "./components/Route/AppRoute";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="mockman" element={<MockmanEs />} />
-      </Routes>
+      <AppRoute />
     </div>
   );
 }
