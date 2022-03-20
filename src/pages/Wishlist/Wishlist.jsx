03dacadb502661d9c70cc5fc5fc6661e1734a21e
gopal <gopalbbharadva@gmail.com>
2022-03-20 07:39:26 +0530

    Feat : Setting logged in user.

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,9 +1,11 @@
 import { Navbar, WishListCard } from "../../barrelexport/Componentutil";
+import { useAuth } from "../../contexts/AuthContext";
 import { checkLoginService } from "../../Services/AuthService";
 import "./Wishlist.css";
 
 export const Wishlist = () => {
-  
+  const { currentUser } = useAuth();
+
   checkLoginService();
   return (
     <div>
