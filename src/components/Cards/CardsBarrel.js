eb3caa3fc590a171778e2782596587309f26addf
gopal <gopalbbharadva@gmail.com>
2022-03-23 10:31:36 +0530

    Feat : toggling wishlist and add to cart

--- src/components/Cards/CardsBarrel.js
+++ src/components/Cards/CardsBarrel.js
@@ -1,5 +1,3 @@
-// export {CartCard} from '../Cards/Cart/CartCard'
 export { HomeCard } from "../Cards/HomeCard/HomeCard";
 export { ProductsListCard } from "../Cards/ProductListingCard/ProductsListCard";
 export { CartCard } from "../Cards/Cart/CartCard";
-export { WishListCard } from "../Cards/WishListCard/WishListCard";
