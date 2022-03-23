529756d9ad830afa56aa9be0a264f43fc6afc6d8
gopal <gopalbbharadva@gmail.com>
2022-03-23 16:30:24 +0530

    Fix : Bracket error resolved

--- src/components/Cards/Cart/CartCard.jsx
+++ src/components/Cards/Cart/CartCard.jsx
@@ -46,34 +46,67 @@ export const CartCard = ({ cartItem }) => {
       }
       removeFromCartHandler();
     }
+  };
 
-    const removeFromCartHandler = async () => {
-      try {
-        const res = await axios.delete(`/api/user/cart/${_id}`, {
+  const removeFromCartHandler = async () => {
+    try {
+      const res = await axios.delete(`/api/user/cart/${_id}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      if (res.status === 200 || res.status === 201) {
+        cartDispatch({
+          type: "REMOVE_FROM_CART",
+          payload: res.data.cart,
+          cartItem,
+        });
+      }
+      toast.error(`${title} removed from cart`, toastProps);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const incrementQuantity = async () => {
+    try {
+      const res = await axios.post(
+        `/api/user/cart/${_id}`,
+        {
+          action: {
+            type: "increment",
+          },
+        },
+        {
           headers: {
             authorization: token,
           },
-        });
-        if (res.status === 200 || res.status === 201) {
-          cartDispatch({
-            type: "REMOVE_FROM_CART",
-            payload: res.data.cart,
-            cartItem,
-          });
         }
-        toast.error(`${title} removed from cart`, toastProps);
-      } catch (error) {
-        console.log(error);
+      );
+      if (res.status === 200 || res.status === 201) {
+        cartDispatch({
+          type: "ADD_TO_CART",
+          payload: res.data.cart,
+          product: cartItem,
+        });
       }
-    };
+      toast.success(`One more ${title} book added `, toastProps);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const incrementQuantity = async () => {
+  const decrementQuantity = async () => {
+    if (quantity === 1) {
+      removeFromCartHandler();
+      return;
+    } else {
       try {
         const res = await axios.post(
           `/api/user/cart/${_id}`,
           {
             action: {
-              type: "increment",
+              type: "decrement",
             },
           },
           {
@@ -84,94 +117,58 @@ export const CartCard = ({ cartItem }) => {
         );
         if (res.status === 200 || res.status === 201) {
           cartDispatch({
-            type: "ADD_TO_CART",
+            type: "DECREMENT_QUANTITY",
             payload: res.data.cart,
             product: cartItem,
           });
         }
-        toast.success(`One more ${title} book added `, toastProps);
+        toast.warn(`Oh! one ${title} book deducted `, toastProps);
       } catch (error) {
         console.log(error);
       }
-    };
+    }
+  };
 
-    const decrementQuantity = async () => {
-      if (quantity === 1) {
-        removeFromCartHandler();
-        return;
-      } else {
-        try {
-          const res = await axios.post(
-            `/api/user/cart/${_id}`,
-            {
-              action: {
-                type: "decrement",
-              },
-            },
-            {
-              headers: {
-                authorization: token,
-              },
-            }
-          );
-          if (res.status === 200 || res.status === 201) {
-            cartDispatch({
-              type: "DECREMENT_QUANTITY",
-              payload: res.data.cart,
-              product: cartItem,
-            });
-          }
-          toast.warn(`Oh! one ${title} book deducted `, toastProps);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    };
-
-    return (
-      <div className="card-hz">
-        <div className="horizontal-image-container">
-          <img className="hr-image" src={imgSrc} alt="No preview available" />
-        </div>
-        <div className="card-body-wrapper">
-          <div className="card-body">
-            <h3 className="card-title">{title}</h3>
-            <small>{author}</small>
-            <p className="card-sell-price">
-              <span>{sellPrice}₹</span>
-              <span className="card-cost-price">{costPrice}₹</span>
-              <span className="card-discount">{discount}%off</span>
-            </p>
-            <div className="quantity-count">
-              <button onClick={incrementQuantity} className="button-count-plus">
-                <span className="flex">
-                  <FiPlus />
-                </span>
-              </button>
-              <p className="quantity-value">{quantity}</p>
-              <button
-                onClick={decrementQuantity}
-                className="button-count-minus"
-              >
-                <span className="flex">
-                  <FiMinus />
-                </span>
-              </button>
-            </div>
-            <div className="card-button-container mt">
-              <button
-                onClick={removeFromCartHandler}
-                className="btn is-btn-danger is-outlined pd-sm"
-              >
-                Remove from cart
-              </button>
-              <button onClick={addToWishList} className="is-secondary pd-sm">
-                Move to favourites
-              </button>
-            </div>
+  return (
+    <div className="card-hz">
+      <div className="horizontal-image-container">
+        <img className="hr-image" src={imgSrc} alt="No preview available" />
+      </div>
+      <div className="card-body-wrapper">
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <small>{author}</small>
+          <p className="card-sell-price">
+            <span>{sellPrice}₹</span>
+            <span className="card-cost-price">{costPrice}₹</span>
+            <span className="card-discount">{discount}%off</span>
+          </p>
+          <div className="quantity-count">
+            <button onClick={incrementQuantity} className="button-count-plus">
+              <span className="flex">
+                <FiPlus />
+              </span>
+            </button>
+            <p className="quantity-value">{quantity}</p>
+            <button onClick={decrementQuantity} className="button-count-minus">
+              <span className="flex">
+                <FiMinus />
+              </span>
+            </button>
+          </div>
+          <div className="card-button-container mt">
+            <button
+              onClick={removeFromCartHandler}
+              className="btn is-btn-danger is-outlined pd-sm"
+            >
+              Remove from cart
+            </button>
+            <button onClick={addToWishList} className="is-secondary pd-sm">
+              Move to favourites
+            </button>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
